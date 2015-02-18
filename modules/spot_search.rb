d5189e89c8cd12b6634d208e899f1133ac69c5a3
Steven Inouye <steven.inouye91@gmail.com>
2015-02-18 10:00:12 -0800

    pre post written -not sure if they work -but it seems like it makes sense

--- modules/spot_search.rb
+++ modules/spot_search.rb
@@ -1,42 +1,76 @@
-require 'rspotify'
-
-
 def play_pause()
   Appscript.app("spotify.app").play
+  @stop = false
   if @@start_time
     elapsed_time_seconds = @@stop_time - @@start_time
     sleep @@song_duration_seconds - elapsed_time_seconds.round(2)
     #calculating the amount of time the song played for before pausing
-    next_song(@@track, @@tracks, @@i)
+    if @i == "random"
+      track = @tracks.shuffle.pop
+      next_song(track, @tracks, "random")
+    else
+      int = @i + 1
+      track = @tracks[int]
+      next_song(track, @tracks, int)
+    end
   end
 end
 
 def stop()
   @@stop_time = Time.now
-  @@stop = true
+  if @@pre == true
+    pre_track.finished()
+  else
+    post_track.finished()
+  end
 end
 
-def next_song(current_track, possible_tracks, i)#plays the current song and gets the next
-    @@track = current_track
-    @@tracks = possible_tracks
-    spot = Appscript.app("spotify.app")
-    uri = current_track.instance_variable_get('@uri')
-    spot.open_location uri #this actualy plays the track
-    ms = current_track.instance_variable_get('@duration_ms')
-    @@i = i++ #have to increase the integer
-    @@start_time = Time.now
-    timer = ms / 60000.to_f
-    @@song_duration_seconds = timer.round(2) * 60
-    sleep @@song_duration_seconds
-  if @@stop == true
-    return
-  elsif i == "random"
-    rand_track = possible_tracks.shuffle.pop
-    next_song(rand_track, possible_tracks, "random")
-  else
-    int = i + 1 #makes 0 to 1 so the next song in the array
-    next_track = possible_tracks[int]
-    next_song(next_track, @@tracks, int)
+#things before new query get hit before new_query
+def new_query()
+  @@pre = true
+end
+
+class Playback
+  require 'rspotify'
+
+  def initialize()
+    @track = 'track'
+    @tracks = 'tracks'
+    @i = 'random'
+    @stop = false
   end
+
+  def finished()
+    @stop = true
+  end
+
+  def next_song(current_track, possible_tracks, i)#plays the current song and gets the next
+    @track = current_track
+    @tracks = possible_tracks
+      spot = Appscript.app("spotify.app")
+      uri = current_track.instance_variable_get('@uri')
+      spot.open_location uri #this actualy plays the track
+      ms = current_track.instance_variable_get('@duration_ms')
+      @i = i #set class variable i
+      @@start_time = Time.now
+      timer = ms / 60000.to_f
+      @@song_duration_seconds = timer.round(2) * 60
+      sleep @@song_duration_seconds
+    if @stop == true
+        return
+    elsif i == "random"
+      post_track = Playback.new
+      @@pre = false
+      rand_track = possible_tracks.shuffle.pop
+      post_track.next_song(rand_track, possible_tracks, "random")
+    else
+      post_track = Playback.new
+      @@pre = false
+      int = i + 1 #have to increase the integer
+      next_track = possible_tracks[int]
+      post_track.next_song(next_track, @tracks, int)
+    end
+  end
+
 end
 
--- modules/spotify.rb
+++ modules/spotify.rb
@@ -16,18 +16,20 @@ def process_spotify(command)
 
   track_name.pop
   search_track = track_name.join(" ")
+
   if command.scan(/run/).length > 0
     play_pause()
   elsif command.scan(/off/).length > 0
       if command.scan(/complete/).length > 0
         spot.stop
-        @@start_time = nil #stops spotify from playing another song afterwards
       else
         spot.pause
       end
     stop()
   end
 
+  pre_track = Playback.new
+  new_query()
   if command.scan(/track/).length > 0
     tracks = RSpotify::Track.search(search_track)
     first_track = tracks.first
@@ -38,24 +40,24 @@ def process_spotify(command)
     art = artists.first
     top_tracks = art.top_tracks(:US)
     rand_track = art.top_tracks(:US).sample
-    next_song(rand_track, top_tracks, "random")
+    pre_track.next_song(rand_track, top_tracks, "random")
   elsif command.scan(/album/).length > 0
     albums = RSpotify::Album.search(search_track)
     album = albums.first
     tracks = album.tracks
     if command.scan(/random/).length > 0
       rand_track = tracks.sample
-      next_song(rand_track, tracks, "random")
+      pre_track.next_song(rand_track, tracks, "random")
     else
       first_track = tracks.first
-      next_song(first_track, tracks, 0)
+      pre_track.next_song(first_track, tracks, 0)
     end
   elsif command.scan(/style/).length > 0
     playlists = RSpotify::Playlist.search(search_track)
     playlist = playlists.sample
     play_tracks = playlist.tracks
     rand_track = playlist.tracks.sample
-    next_song(rand_track, play_tracks, "random")
+    pre_track.next_song(rand_track, play_tracks, "random")
   end
 
 end
